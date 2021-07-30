e3e475e868eb618e85255ca572fc327e9572c03a
Iyke Israel <iykeprince48@gmail.com>
2021-07-30 22:53:28 +0100

    Added time formatting using momentjs

--- public/js/index.js
+++ public/js/index.js
@@ -11,17 +11,19 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', (message) => {
     console.log('newMessage', message);
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
-    li.text(`${message.from}: ${message.text}`);
+    li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
     jQuery('#messages').append(li);
 })
 
 socket.on('newLocationMessage', function(message){
+    var formattedTime = moment(message.createdAt).format('h:mm a');
     var li = jQuery('<li></li>');
     var a = jQuery('<a target="_blank">My current location</a>');
 
-    li.text(`${message.from}: `);
+    li.text(`${message.from} ${formattedTime}: `);
     a.attr('href', message.url);
     li.append(a);
     jQuery('#messages').append(li);
@@ -36,9 +38,8 @@ jQuery('#message-form').on('submit', function(e){
     socket.emit('createMessage', {
         from: 'User',
         text: messageTextbox.val()
-    }, function(message){
+    }, function(){
         messageTextbox.val('');
-        console.log(message)
     });
 })
 
@@ -48,12 +49,16 @@ locationButton.on('click', function(){
         return alert('Geolocation not supported by your browser');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function(position){
        socket.emit('createLocationMessage', {
            latitude: position.coords.latitude,
            longitude: position.coords.longitude
        })
+       locationButton.removeAttr('disabled').text('Send location');
     }, function(error){
         alert('Unable to fetch location');
+        locationButton.removeAttr('disabled').text('Send location');
     })
 })
